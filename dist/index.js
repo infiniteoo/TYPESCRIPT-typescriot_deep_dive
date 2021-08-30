bfc5e821b810659914ba4a274357986f28e4a362
Troy Dorman <troydorman@gmail.com>
2021-08-30 11:05:08 -0600

    covered the basics of unions and enums

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,36 @@
 "use strict";
+// basic types
 let id = 5;
-console.log("ID:", id);
+let company = "Troversy Media";
+let isPublished = true;
+let x = "hello";
+let age;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "Hello"];
+// Tuple
+let person = [1, "Troy", true];
+// Tuple Array
+let employee;
+employee = [
+    [1, "Troy"],
+    [2, "Jill"],
+    [3, "John"],
+];
+// union
+let pid;
+pid = "22";
+// Enums
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
