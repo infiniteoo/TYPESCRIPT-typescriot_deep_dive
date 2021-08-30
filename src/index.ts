a3b9cb1abe34f19579438bf62c7a1b53938465ca
Troy Dorman <troydorman@gmail.com>
2021-08-30 11:02:18 -0600

    covered the basics of types, arrays and tuples

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+// basic types
 let id: number = 5;
+let company: string = "Troversy Media";
+let isPublished: boolean = true;
+let x: any = "hello";
+let age: number;
 
-console.log("ID:", id);
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, "Hello"];
+
+// Tuple
+let person: [number, string, boolean] = [1, "Troy", true];
+
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+    [1, "Troy"],
+    [2, "Jill"],
+    [3, "John"],
+]
